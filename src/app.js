c2f4dabc22ec7668d316684b7f64f2321c780e72
MUDELY SYLVIA SAMBO <91378831+mudelysambo@users.noreply.github.com>
2022-09-28 10:03:22 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,93 @@
+const calculateBtn = document.getElementById('calc-btn');
+const resetBtn = document.getElementById('reset-btn');
+const heightInput = document.getElementById('height-input');
+const weigthInput = document.getElementById('weight-input');
+
+const resetInputs = () => {
+    heightInput.value = '';
+    weigthInput.value = '';
+
+}
+
+const calculateBmi = () => {
+  const enteredHeight = +heightInput.value;
+  const enteredweight = +weigthInput.value;
+
+  const bmi =  enteredweight / (enteredHeight * enteredHeight);
+
+  console.log(bmi);
+};
+
+
+
+calculateBtn.addEventListener('click',calculateBmi);
+resetBtn.addEventListener('click',resetInputs);const calculateBtn = document.getElementById('calc-btn');
+const resetBtn = document.getElementById('reset-btn');
+const heightInput = document.getElementById('height-input');
+const weigthInput = document.getElementById('weight-input');
+
+const resetInputs = () => {
+    heightInput.value = '';
+    weigthInput.value = '';
+
+}
+
+const calculateBmi = () => {
+  const enteredHeight = +heightInput.value;
+  const enteredweight = +weigthInput.value;
+
+  const bmi =  enteredweight / (enteredHeight * enteredHeight);
+
+  console.log(bmi);
+};
+
+
+
+calculateBtn.addEventListener('click',calculateBmi);
+resetBtn.addEventListener('click',resetInputs);const calculateBtn = document.getElementById('calc-btn');
+const resetBtn = document.getElementById('reset-btn');
+const heightInput = document.getElementById('height-input');
+const weigthInput = document.getElementById('weight-input');
+
+const resetInputs = () => {
+    heightInput.value = '';
+    weigthInput.value = '';
+
+}
+
+const calculateBmi = () => {
+  const enteredHeight = +heightInput.value;
+  const enteredweight = +weigthInput.value;
+
+  const bmi =  enteredweight / (enteredHeight * enteredHeight);
+
+  console.log(bmi);
+};
+
+
+
+calculateBtn.addEventListener('click',calculateBmi);
+resetBtn.addEventListener('click',resetInputs);const calculateBtn = document.getElementById('calc-btn');
+const resetBtn = document.getElementById('reset-btn');
+const heightInput = document.getElementById('height-input');
+const weigthInput = document.getElementById('weight-input');
+
+const resetInputs = () => {
+    heightInput.value = '';
+    weigthInput.value = '';
+
+}
+
+const calculateBmi = () => {
+  const enteredHeight = +heightInput.value;
+  const enteredweight = +weigthInput.value;
+
+  const bmi =  enteredweight / (enteredHeight * enteredHeight);
+
+  console.log(bmi);
+};
+
+
+
+calculateBtn.addEventListener('click',calculateBmi);
+resetBtn.addEventListener('click',resetInputs);
